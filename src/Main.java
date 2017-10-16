6c0e8a47672f6e8e160fefcf7d569a49777bea5c
DanielBerw <daniel.berwanger@hotmail.de>
2017-10-16 16:02:21 +0200

    Hello world program with simple sensor interaction for test purposes. Not run on EV3 yet.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.TouchAdapter;
 
 public class Main {
 
+	static EV3TouchSensor t = new EV3TouchSensor(SensorPort.S1);
+	static TouchAdapter touch = new TouchAdapter(t);
+
+	
+	//hello world tester
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
 
+		//wait for press on touch sensor
+		while(!(touch.isPressed())){
+			//IDLE
+		}
+		
+		System.out.println("Thank you.");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
