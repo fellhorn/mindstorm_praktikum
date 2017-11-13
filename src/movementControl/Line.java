0a25e04a0c2ba4931a39251461e0eca08fcfd193
Tarek Wilkening <tarek_wilkening@web.de>
2017-11-13 10:42:55 +0100

    refactor line

--- src/movementControl/Line.java
+++ src/movementControl/Line.java
@@ -12,13 +12,21 @@ public class Line extends AbstractInterruptableStateRunner {
 	private DebugMessages message = new DebugMessages(1);
 	private EV3ColorSensor col = new EV3ColorSensor(SensorPort.S2);  
 	private EV3GyroSensor gyro = new EV3GyroSensor(SensorPort.S1);
-	private boolean lastLossRight = true;  //TODO should be set according to first curve 
-	private boolean rotating = false;
-	private boolean turnBack = false;
-	private boolean adjust = false;
-	private boolean assumeGap  = false;
+	private enum LineStates {
+		ON_LINE,
+		TURN_BACK_RIGHT,
+		TURN_BACK_LEFT,
+		SEARCH_LINE_LEFT,
+		SEARCH_LINE_RIGHT,
+		LINE_LOST_RIGHT,
+		LINE_LOST_LEFT,
+		ON_GAP,
+		ERROR
+	} 
+	private LineStates lineState;
 	private float[] rotDegree = new float[] {0.0f, 0.0f};
 	
+	private static final int DEFAULT_CURVE_SPEED = 400;
 	private static final float SEARCH_ROTATION_TOLERANCE = 5.0f; 
 
 	
@@ -32,6 +40,7 @@ public class Line extends AbstractInterruptableStateRunner {
 		message.clear();
 		message.echo("Following white line.");
 		StraightLines.startEngines(450);
+		lineState = LineStates.ON_LINE;
 	}
 
 	@Override
@@ -39,21 +48,19 @@ public class Line extends AbstractInterruptableStateRunner {
 		int groundColor = col.getColorID(); 
 		switch (groundColor) {
 		case Color.WHITE:
+		case Color.BLUE:
 			//reset state variables to "on line"
-			rotating = false;
-			assumeGap = false;
-			lastLossRight  = lastLossRight != turnBack; //change lLR iff robot searched in wrong direction first
-			turnBack = false;
-			adjust = false;
+			lineState = LineStates.ON_LINE;
 			StraightLines.regulatedForwardDrive(450);
 			//TODO ENHANCEMENT speedup if line was straight for some time
 			break;
 		case Color.BLACK:
-			if(!assumeGap){
-				searchLine();				
-			} else {
+		case Color.BROWN:
+			if(lineState == LineStates.ON_GAP){
 				StraightLines.regulatedForwardDrive(450);
 				//TODO what if the robot does not find the end of line after gap?
+			} else {
+				searchLine();				
 			}
 			break;
 		case Color.RED:
@@ -63,7 +70,7 @@ public class Line extends AbstractInterruptableStateRunner {
 			//TODO think of better error case behavior
 			//stop robot if measurement error occurs
 			System.out.println("\n" + groundColor);
-			running = false; 
+			lineState = LineStates.ERROR;
 			break;
 		}
 	}
@@ -74,68 +81,67 @@ public class Line extends AbstractInterruptableStateRunner {
 		StraightLines.stop();
 	}
 	
-	private void searchLine(){
-		//Just lost line: get initial rotation position
-		if(!rotating) {
-			gyro.getAngleMode().fetchSample(rotDegree, 0);
-			rotating = true;
+	private float distance(float a, float b) {
+		return a < b ? b-a : a-b;
+	}
+	private boolean angleGreater90(float a, float b) {
+		return distance(a, b) >= 90 + SEARCH_ROTATION_TOLERANCE;
+	}
+	private boolean changeStateIf90(LineStates lineState) {
+		if (angleGreater90(rotDegree[0], rotDegree[1])) {
+			StraightLines.stop();
+			this.lineState = lineState;
+			return true;
 		}
+		return false;
+	}
+	
+	private void searchLine(){
+		// Get current angle
 		gyro.getAngleMode().fetchSample(rotDegree, 1);
-		if(lastLossRight) {
-			if(!turnBack) {
-				//search for line on the left
-				Curves.smoothSpeededLeftTurn(-1, 450);	
-				if (rotDegree[0] - rotDegree[1] < -90.0 - SEARCH_ROTATION_TOLERANCE) {
-					turnBack = true;
-					StraightLines.stop();
-				}
-			} else if(turnBack && (rotDegree[0] - rotDegree[1] < 0.0)){
-				//line not found => you can turn back quicker
-				Curves.smoothSpeededRightTurn(-1, 900);	
-			} else if(turnBack && !adjust){
-				//search for line on the right
-				Curves.smoothSpeededRightTurn(-1, 450);
-				if (rotDegree[0] - rotDegree[1] > 90.0 + SEARCH_ROTATION_TOLERANCE) {
-					adjust = true;
-					StraightLines.stop();
-				}
-			} else if(adjust && (rotDegree[0] - rotDegree[1] > 0.0)) {
-				//line not found => you can turn back to original position quicker
-				Curves.smoothSpeededLeftTurn(-1, 900);
-			} else {
-				//no line here => must be gap
-				adjust = false;
-				rotating = false;
-				assumeGap = true;
+		
+		switch (lineState) {
+		case LINE_LOST_LEFT:
+			// Search left first
+			gyro.getAngleMode().fetchSample(rotDegree, 0);
+			lineState = LineStates.SEARCH_LINE_LEFT;
+			break;
+		case LINE_LOST_RIGHT:
+			// Search right first
+			gyro.getAngleMode().fetchSample(rotDegree, 0);
+			lineState = LineStates.SEARCH_LINE_RIGHT;
+			break;
+		case SEARCH_LINE_LEFT:
+			if (changeStateIf90(LineStates.TURN_BACK_LEFT)) {
+				break;
 			}
-
-		} else {
-			if(!turnBack) {
-				//search for line on the right
-				Curves.smoothSpeededRightTurn(-1, 450);	
-				if (rotDegree[0] - rotDegree[1] > 90.0 + SEARCH_ROTATION_TOLERANCE) {
-					turnBack = true;
-					StraightLines.stop();
-				}
-			} else if(turnBack && (rotDegree[0] - rotDegree[1] > 0.0)){
-				//line not found => you can turn back quicker
-				Curves.smoothSpeededLeftTurn(-1, 900);	
-			} else if(turnBack && !adjust){
-				//search for line on the left
-				Curves.smoothSpeededLeftTurn(-1, 450);
-				if (rotDegree[0] - rotDegree[1] < -90.0 + SEARCH_ROTATION_TOLERANCE) {
-					adjust = true;
-					StraightLines.stop();
-				}
-			} else if(adjust && (rotDegree[0] - rotDegree[1] < 0.0)) {
-				//line not found => you can turn back to original position quicker
-				Curves.smoothSpeededRightTurn(-1, 900);
-			} else {
-				//no line here => must be gap
-				adjust = false;
-				rotating = false;
-				assumeGap = true;
+			Curves.smoothSpeededLeftTurn(-1, DEFAULT_CURVE_SPEED);
+			break;
+		case TURN_BACK_LEFT:
+			if (changeStateIf90(LineStates.SEARCH_LINE_RIGHT)) {
+				break;
 			}
+			Curves.smoothSpeededRightTurn(-1, DEFAULT_CURVE_SPEED);
+			break;
+		case SEARCH_LINE_RIGHT:
+			if (changeStateIf90(LineStates.TURN_BACK_RIGHT)) {
+				break;
+			}
+			Curves.smoothSpeededRightTurn(-1, DEFAULT_CURVE_SPEED);
+			break;
+		case TURN_BACK_RIGHT:
+			if (changeStateIf90(LineStates.ON_GAP)) {
+				break;
+			}
+			Curves.smoothSpeededLeftTurn(-1, DEFAULT_CURVE_SPEED);
+			break;
+		case ON_LINE:
+			StraightLines.regulatedForwardDrive(450);
+			break;
+		case ON_GAP:
+			break;
+			default:
+				// Error
 		}
 	}
 }
