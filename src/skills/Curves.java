f3f915ec2911313e9271bac36fe68cb29c309344
Tarek Wilkening <tarek_wilkening@web.de>
2017-11-27 11:44:35 +0100

    add methods for 90 degree turns

--- src/skills/Curves.java
+++ src/skills/Curves.java
@@ -4,9 +4,35 @@ import lejos.utility.DebugMessages;
 
 public class Curves {
 	
-
+	private static final int TURN_SPEED = 200;
 	private static DebugMessages message = new DebugMessages(4);
 	
+	public static void turnRight90() {
+		float [] sample = {0f,0f};
+		Sensors.getGyro().getAngleMode().fetchSample(sample, 0);
+		Sensors.getGyro().getAngleMode().fetchSample(sample, 1);
+		smoothSpeededRightTurn(-1, TURN_SPEED);
+		
+		while (sample[0] - sample[1] < 90) {
+			Sensors.getGyro().getAngleMode().fetchSample(sample, 1);
+		}
+		StraightLines.stop();
+		message.echo(sample[0] + ", " + sample[1]);
+
+	}
+	
+	public static void turnLeft90() {
+		float [] sample = {0f,0f};
+		Sensors.getGyro().getAngleMode().fetchSample(sample, 0);
+		Sensors.getGyro().getAngleMode().fetchSample(sample, 1);
+		smoothSpeededLeftTurn(-1, TURN_SPEED);
+		
+		while (sample[1] - sample[0] < 90) {
+			Sensors.getGyro().getAngleMode().fetchSample(sample, 1);
+		}
+		StraightLines.stop();
+		message.echo(sample[0] + ", " + sample[1]);
+	}
 	
 	public static void smoothSpeededRightTurn(float smoothness, float speed) {
 		StraightLines.getRight().setSpeed(speed);
